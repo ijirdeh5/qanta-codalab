41e640f8b4c5513972a6c558bbb8a558cb321593
CBSkarmory <CBSkarmory@users.noreply.github.com>
2018-11-14 18:36:48 -0500

    changed obscurity interface

--- src/qanta/obscurity/obscurity.py
+++ src/qanta/obscurity/obscurity.py
@@ -1,2 +1,12 @@
-def get_obscurity(word):
-    return len(word)
+import pickle
+
+# This runs on first import
+with open('wiki_wc.pickle', 'rb') as f:
+    wc = pickle.dump(f)
+
+
+def get_article_len(title: str) -> int:
+    """"
+    :return the word count of the article corresponding to the title given if found, or None if not found
+    """
+    return wc[title] if title in wc else None
