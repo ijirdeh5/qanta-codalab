25ba1c629bdd4ba167c080321be40797fcbb9da5
kevchn <kevchen98@gmail.com>
2018-11-14 19:00:40 -0500

    fix typo

--- src/qanta/obscurity/preprocess.py
+++ src/qanta/obscurity/preprocess.py
@@ -1,28 +1,29 @@
-WIKI_FILE_PATH = '../../../data/wiki_lookup.json'
-
 import json
 import pickle
 
-word_counts = {}
+def pickle_word_counts_wiki():
+    WIKI_FILE_PATH = '../../../data/wiki_lookup.json'
 
-print("Loading file...")
-with open(WIKI_FILE_PATH) as f:
-    wiki = json.load(f)
+    word_counts = {}
 
-    iterations = 0
-    total_iterations = len(wiki.keys())
+    print("Loading file...")
+    with open(WIKI_FILE_PATH) as f:
+        wiki = json.load(f)
 
-    for article in wiki.keys():
-        text = wiki[article]['text']
-        word_count = len(text.split())
-        word_counts[article] = word_count
+        iterations = 0
+        total_iterations = len(wiki.keys())
 
-        iterations += 1
-        if iterations % 1000 == 0:
-            print("Processing: " + "{:.0%}".format(iterations / total_iterations))
+        for article in wiki.keys():
+            text = wiki[article]['text']
+            word_count = len(text.split())
+            word_counts[article] = word_count
 
-with open('word_count.pickle', 'wb') as handle:
-    pickle.dump(word_counts, handle)
+            iterations += 1
+            if iterations % 1000 == 0:
+                print("Processing: " + "{:.0%}".format(iterations / total_iterations))
 
+    with open('word_count.pickle', 'wb') as handle:
+        pickle.dump(word_counts, handle)
 
-    
+if __name__ == '__main__':
+    pickle_word_counts_wiki()
