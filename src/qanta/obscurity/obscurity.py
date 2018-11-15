3b110d6e2544b9f0b39d37fb6e48de24afb509c7
CBSkarmory <CBSkarmory@users.noreply.github.com>
2018-11-14 19:35:57 -0500

    log obscurity

--- src/qanta/obscurity/obscurity.py
+++ src/qanta/obscurity/obscurity.py
@@ -1,4 +1,5 @@
 import pickle
+import numpy as np
 from typing import Optional
 
 # This runs on first import
@@ -11,3 +12,15 @@ def get_article_len(title: str) -> Optional[int]:
     :return the word count of the article corresponding to the title given if found, or None if not found
     """
     return wc[title] if title in wc else None
+
+
+def get_log_wc(title: str) -> Optional[float]:
+    """
+    :param title: the Article Name
+    :return: the log (ln) of the wc
+    """
+    num_words = get_article_len(title)
+    if num_words is not None:
+        return np.log(num_words)
+    else:
+        return None
